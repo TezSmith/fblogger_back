46b489f9fbe3dca17153bda706d94369a777fd36
Tez Smith <montez.smith@flatironschool.com>
2019-02-04 18:03:24 -0500

    Added Seed Data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
 Student.create(name: "Vanessa", email: "Vanessa.prema@flatironschool.com", github: "Vanessa@github.com", blog: "Vanessa@Medium.com", password: "test")
+Student.create(name: "Sarah", email: "Sarah.Yun@flatironschool.com", github: "sarah@github.com", blog: "Sarah@Medium.com", password: "test")
+Student.create(name: "Brian", email: "Brian.Cho@flatironschool.com", github: "brian@github.com", blog: "Brian@Medium.com", password: "test")
+Instructor.create(name: "Tez", email: "Montez.Smith@flatironschool.com", github: "tez@github.com", password:"test")
+Instructor.create(name: "Brit", email: "Brit.butler@flatironschool.com", github: "brit@github.com", password: "test")
+Cohort.create(name: "Sexy And I Know Git", location: "Atlanta", student_id: 1, instructor_id: 1)
+Cohort.create(name: "Sexy And I Know Git", location: "Atlanta", student_id: 2, instructor_id: 2)
+Cohort.create(name: "Sexy And I Know Git", location: "Atlanta", student_id: 3, instructor_id: 1)
