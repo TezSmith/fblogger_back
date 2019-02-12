473435d8c5d102c461cbaf4e673493ebd773d140
Tez Smith <montez.smith@flatironschool.com>
2019-02-12 11:49:11 -0500

    Updated Cors

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
+  has_secure_password
 
   # has_many :cohort_instructors, foreign_key: :student_id
   # has_many :instructors, through: :cohorts, source: :instructor
-  #
+
   # has_many :cohorts, foreign_key: :instructor_id
   # has_many :students, through: :cohorts, source: :student
 
