776de5af2e88e21a66f11933fe10d6dcb76861b9
TezSmith <Montez.Smith1@gmail.com>
2019-02-12 21:45:03 -0500

    Added Route Updates

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,14 +1,6 @@
 module Api
  module V1
    class SessionsController < ApplicationController
-<<<<<<< HEAD
-      def create
-      end
-
-      def destroy
-      end 
-    end
-=======
 
      def new
      end
@@ -17,6 +9,5 @@ module Api
      end
 
    end
->>>>>>> 476d8361c76c43e61466c40e06f632ccac3d23f7
   end
 end
