73ec11c95027aea454297ccfd64107713cdc163c
TezSmith <Montez.Smith1@gmail.com>
2019-02-13 22:47:29 -0500

    Testing Params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,14 @@
 class UsersController < ApplicationController
+
+  def create
+    byebug
+    @user = User.new(user_params)
+  end
+
+  private
+
+  def user_params
+   params.require(:user).permit(:first_name, :last_name, :email )
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
 
+resources :users
+
   namespace :api do
    namespace :v1 do
 
-      resources :students, only: [:index, :show]
-      resources :instructors, only: [:index, :show]
+      # resources :students, only: [:index, :show]
+      # resources :instructors, only: [:index, :show]
       resources :cohorts, only: [:index, :show]
 
     end
