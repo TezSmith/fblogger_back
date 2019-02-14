a713fbc06c5ecc6bba558e0f91774f8028354ee2
Tez Smith <montez.smith@flatironschool.com>
2019-02-14 17:01:26 -0500

    updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,13 +7,13 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 User.destroy_all
 
-Instructor.create!(first_name: "Tez", last_name: "Smith", email: "Montez.Smith@flatironschool.com", password:"Ok", github: "http://github.com/TezSmith", blog: "https://medium.com/@montezsmith")
-Instructor.create!(first_name: "Safae", last_name: "Alaoui", email: "Safae.Alaoui@flatironschool.com", password:"Ok", github: "http://github.com/Safae", blog: "")
-Instructor.create!(first_name: "Roniece", last_name: "Ricardo", email: "Roniece.Ricardo@flatironschool.com", password:"Ok", github: "https://github.com/ronricardo/", blog: "https://medium.com/@roniecericardo")
+Instructor.create!(first_name: "Tez", last_name: "Smith", email: "Montez.Smith@flatironschool.com", password:"1234", github: "http://github.com/TezSmith", blog: "https://medium.com/@montezsmith")
+Instructor.create!(first_name: "Safae", last_name: "Alaoui", email: "Safae.Alaoui@flatironschool.com", password:"1234", github: "http://github.com/Safae", blog: "")
+Instructor.create!(first_name: "Roniece", last_name: "Ricardo", email: "Roniece.Ricardo@flatironschool.com", password:"1234", github: "https://github.com/ronricardo/", blog: "https://medium.com/@roniecericardo")
 
-Student.create!(first_name: "Vanessa", last_name: "Prema", email: "Vanessa.Prema@flatironschool.com", password:"Ok", github:"https://github.com/vprema94", blog:"https://medium.com/@vanessaprema")
-Student.create!(first_name: "Blair", last_name: "Carroll", email: "Blair.Carroll@flatironschool.com", password:"Ok", github:"https://github.com/bcarrol2", blog:"https://medium.com/@blairacarroll")
-Student.create!(first_name: "Kaeland", last_name: "Chatman", email: "Kaeland.Chatman@flatironschool.com", password:"Ok", github:"https://github.com/kaeland", blog:"https://medium.com/@kaeland1")
+Student.create!(first_name: "Vanessa", last_name: "Prema", email: "Vanessa.Prema@flatironschool.com", password:"1234", github:"https://github.com/vprema94", blog:"https://medium.com/@vanessaprema")
+Student.create!(first_name: "Blair", last_name: "Carroll", email: "Blair.Carroll@flatironschool.com", password:"1234", github:"https://github.com/bcarrol2", blog:"https://medium.com/@blairacarroll")
+Student.create!(first_name: "Kaeland", last_name: "Chatman", email: "Kaeland.Chatman@flatironschool.com", password:"1234", github:"https://github.com/kaeland", blog:"https://medium.com/@kaeland1")
 
 Cohort.create!(name: "Sexy and I Know Git", location: "Atlanta", instructor_id: 1, student_id: 4)
 Cohort.create!(name: "Sexy and I Know Git", location: "Atlanta", instructor_id: 2, student_id: 5)
