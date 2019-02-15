6841476b49c5ada47a234fc8cfa9fafc479c5d73
TezSmith <Montez.Smith1@gmail.com>
2019-02-14 21:25:15 -0500

    Converted Rails app to string since variable wasn't set

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-   params.permit(:first_name, :last_name, :password, :email )
+   params.permit(:first_name, :last_name, :password, :email)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  # get 'tokens/create'
+  get 'tokens/create'
 
 resources :users
 resources :tokens, only: [:create]
